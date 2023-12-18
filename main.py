4c8c0e2bd8d5b0f688afa5a5982eab441207dc42
Vince <v.coccina@orange.fr>
2023-12-18 11:09:29 +0100

    Affinage stratégie

--- main.py
+++ main.py
@@ -5,42 +5,40 @@ import plotly.graph_objects as go
 import os
 from binance.client import Client
 from binance.exceptions import BinanceAPIException, BinanceOrderException
-from binance import ThreadedWebsocketManager
-from dotenv import load_dotenv
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 import time
 
 load_dotenv()
 
 class Trader():
-    def __init__(self, symbol, bar_length, stop_loss, target_profit, change):
+    def __init__(self, symbol, bar_length, stop_loss, target_profit, change, units):
         self.symbol = symbol
         self.bar_length = bar_length
         self.available_intervals = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
         self.stop_loss = stop_loss
         self.target_profit = target_profit
         self.change = change
-        self.last_processed = None
+        self.units = units
+        self.trades = pd.DataFrame(columns=['Time',"Symbol", 'Type', 'OrderID', 'Price', 'Quantity','Total', 'Status'])
     
     def start_trading(self):
 
-        self.twm = ThreadedWebsocketManager()
-        self.twm.start()
-
         if self.bar_length in self.available_intervals:
-            self.get_recent(symbol = self.symbol, interval = self.bar_length)
-            self.twm.start_kline_socket(callback = self.stream_candles,
-                                        symbol = self.symbol, interval = self.bar_length)
-    
+            self.get_recent(symbol=self.symbol, interval=self.bar_length)
+            self.execute_trades()
+
     def get_recent(self, symbol, interval):
         
         start = "2023-01-01"
 
         df = pd.DataFrame(client.get_historical_klines(symbol=symbol, interval=interval, start_str=start))
-        df = df.iloc[:,:6]
-        df.columns = ["Time", "Open", "High", "Low", "Close", "Volume"]
-        df.set_index("Time", inplace=True)
-        df.index = pd.to_datetime(df.index, unit="ms")
+        df["Date"] = pd.to_datetime(df.iloc[:,0], unit = "ms")
+        df.columns = ["Open Time", "Open", "High", "Low", "Close", "Volume",
+                      "Clos Time", "Quote Asset Volume", "Number of Trades",
+                      "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume", "Ignore", "Date"]
+        df = df[["Date", "Open", "High", "Low", "Close", "Volume"]].copy()
+        df.set_index("Date", inplace = True)
         df = df.astype(float)
         self.data = df
         self.calculate_indicators()
@@ -79,44 +77,41 @@ class Trader():
 
         return ((pd.Series(profits) + 1).prod() - 1)
     
-    def stream_candles(self, msg):
-        event_time = pd.to_datetime(msg["E"], unit = "ms")
-        start_time = pd.to_datetime(msg["k"]["t"], unit = "ms")
-        first   = float(msg["k"]["o"])
-        high    = float(msg["k"]["h"])
-        low     = float(msg["k"]["l"])
-        close   = float(msg["k"]["c"])
-        volume  = float(msg["k"]["v"])
-        print("Time: {} | Price: {}".format(event_time, close))
-        new_row = pd.DataFrame({
-        "Open": [first],
-        "High": [high],
-        "Low": [low],
-        "Close": [close],
-        "Volume": [volume],
-        }, index=[start_time])
+    
+    def execute_trades(self):
+        last_row = self.data.iloc[-1]
+        index = last_row.name  # Cela donne l'index de la dernière ligne, c'est-à-dire la date/heure
+        signal = last_row["Signal"]
 
-        new_data = pd.DataFrame(new_row, index=[pd.to_datetime(msg["k"]["t"], unit="ms")])
-        self.process_new_data(new_data)
-    
-    def process_new_data(self, new_data):
-        self.data = pd.concat([self.data, new_data])
-        self.calculate_indicators()
-        self.execute_trades(new_data)
-    
-    def execute_trades(self, new_data):
-        for index, row in new_data.iterrows():
-            if self.last_processed is not None and index <= self.last_processed:
-                continue
-            signal = row.get("Signal", 0)
-            try:
-                if signal == 1:
-                    print(f"Achat effectué à {index}")
-                elif signal == -1:
-                    print(f"Vente effectuée à {index}")
-            except Exception as e:
+        try:
+            if signal == 1:
+                order = client.create_order(symbol=self.symbol, side="BUY", type="MARKET", quantity=self.units)
+                print(f"Vente effectuée à : ", order)
+                self.record_trade(order, 'BUY')
+            elif signal == -1:
+                order = client.create_order(symbol=self.symbol, side="SELL", type="MARKET", quantity=self.units)
+                print(f"Vente effectuée à : ", order)
+                self.record_trade(order, 'SELL')
+            else:
+                print(f"neutre")
+        except Exception as e:
                 print(f"Erreur: {e}")
-            self.last_processed = index
+
+
+    def record_trade(self, order, type, time):
+        price = float(order['fills'][0]['price'])
+        quantity = float(order['fills'][0]['qty'])
+        new_trade = {
+            'Time': time,
+            "Symbol": symbol,
+            'Type': type,
+            'OrderID': order['orderId'],
+            'Price': price,
+            'Quantity': quantity,
+            'Total': price * quantity,
+            'Status': order['status']
+        }
+        self.trades = self.trades._append(new_trade, ignore_index=True)
 
 
 if __name__ == "__main__": # Lance le script seulement si main.py est appelé
@@ -130,24 +125,31 @@ if __name__ == "__main__": # Lance le script seulement si main.py est appelé
     
     # Variables de trading
     bar_length = "1h"
-    symbol="BTCUSDT"
+    symbol = "BTCUSDT"
+    units = 0.0004
     change = 0.02
     target_profit = 1.01
-    stop_loss = 0.99 
+    stop_loss = 0.99
 
     # Instance de la class Trader
-    trader = Trader(symbol=symbol, bar_length=bar_length, stop_loss=stop_loss, target_profit=target_profit, change=change)
+    trader = Trader(symbol=symbol, bar_length=bar_length, stop_loss=stop_loss, target_profit=target_profit, change=change, units=units)
+    first_run = True
+    try:
+        while True:
+            if first_run:
+                trader.start_trading()
+                first_run = False
 
-    # Lance le script
-    trader.start_trading()
-
-    run_time = 60  # Durée d'exécution en secondes
-    time.sleep(run_time)
-
-    # Arrêt du WebSocket
-    trader.twm.stop()
+            current_time = datetime.utcnow()
+            next_hour = current_time.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+            sleep_time = (next_hour - current_time).total_seconds()
+            time.sleep(sleep_time)
+            trader.start_trading()
+    except KeyboardInterrupt:
+        print("Arrêt du script...")
 
     filtered_data = trader.data[trader.data["Signal"]!=0]
     print(filtered_data)
+    print(trader.trades[50:])
 
 
